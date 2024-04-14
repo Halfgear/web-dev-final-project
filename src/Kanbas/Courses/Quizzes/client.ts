91020bf6115fababe7f06247a8d57bdbd16172ee
HwiJoon Lee <107892477+Halfgear@users.noreply.github.com>
2024-04-13 23:54:57 -0400

    added comments and organized code

--- src/Kanbas/Courses/Quizzes/client.ts
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -4,33 +4,23 @@ const QUIZ_API = `${API_BASE}/api/quizzes`;
 axios.defaults.withCredentials = true;
 
 export const findCourseQuizzes = async (courseId: string) => {
-  const response = await axios.get(
-    `${QUIZ_API}/${courseId}/quizzes`,
-  );
+  const response = await axios.get(`${QUIZ_API}/${courseId}/quizzes`);
   return response.data;
 };
 export const createQuiz = async (courseId: any, quiz: any) => {
-  const response = await axios.post(
-    `${QUIZ_API}/${courseId}/quizzes`,
-    quiz,
-  );
+  const response = await axios.post(`${QUIZ_API}/${courseId}/quizzes`, quiz);
   return response.data;
 };
 export const deleteQuiz = async (quizId: any) => {
-    const response = await axios.delete(
-      `${QUIZ_API}/${quizId}`,
-    );
-    return response.data;
-  };
-  
+  const response = await axios.delete(`${QUIZ_API}/${quizId}`);
+  return response.data;
+};
+
 export const updateQuiz = async (quiz: any) => {
-  const response = await axios.put(
-    `${QUIZ_API}/${quiz._id}`,
-    quiz,
-  );
+  const response = await axios.put(`${QUIZ_API}/${quiz._id}`, quiz);
   return response.data;
 };
 export const findQuizById = async (quizId: string) => {
-    const response = await axios.get(`${QUIZ_API}/${quizId}`);
-    return response.data;
-  };
+  const response = await axios.get(`${QUIZ_API}/${quizId}`);
+  return response.data;
+};
